415db716a082ea3fb5fe5bae447d8df81765d9e7
xaqan05 <xaqan2005@gmail.com>
2025-07-15 03:34:45 +0400

    Created RegisterDTO

---
+++ src/Core/Eciton.Application/DTOs/Auth/RegisterDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Eciton.Application.DTOs.Auth;
+public class RegisterDTO
+{
+    public string Name { get; set; } = null!;
+    public string Surname { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string ConfirmPassword { get; set; } = null!;
+}
