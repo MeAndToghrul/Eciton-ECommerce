68f70d4e338be4c98d37e379a8e3eea832d339c1
xaqan05 <xaqan2005@gmail.com>
2025-07-15 04:27:46 +0400

    Created Base,NotFound and Exist Exception

---
+++ src/Core/Eciton.Application/Exceptions/Commons/ExistException.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Eciton.Application.Exceptions.Commons;
+public class ExistException<T> : Exception,IBaseException
+{
+    public int Code => StatusCodes.Status409Conflict;
+    public string ErrorMessage { get; }
+    const string _message = " is exist";
+
+    public ExistException() : base(typeof(T).Name + _message)
+    {
+        ErrorMessage = typeof(T).Name + _message;
+    }
+
+    public ExistException(string message) : base(message)
+    {
+        ErrorMessage = message;
+    }
+}
