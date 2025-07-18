24b5c95b01bd7a551ac2fe7b97c919470482ced0
Toghrul-Mehdi <togrulcm-bp215@code.edu.az>
2025-07-18 21:53:54 +0400

    CREATED VerifyEmail with JWT & UPDATED Email Template

---
+++ src/Core/Eciton.Application/Commands/Auth/VerifyEmailCommand.cs
@@ -0,0 +1,15 @@
+﻿using Eciton.Application.ResponceObject;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eciton.Application.Commands.Auth
+{
+    public class VerifyEmailCommand : IRequest<Response>
+    {
+        public string Token { get; set; } = null!;
+    }
+}
---
+++ src/Core/Eciton.Application/Handlers/Auth/VerifyEmailCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Eciton.Application.Abstractions;
+using Eciton.Application.Commands.Auth;
+using Eciton.Application.ResponceObject;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eciton.Application.Handlers.Auth
+{
+    public class VerifyEmailCommandHandler : IRequestHandler<VerifyEmailCommand, Response>
+    {
+        private readonly IAuthService _authService;
+
+        public VerifyEmailCommandHandler(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public async Task<Response> Handle(VerifyEmailCommand request, CancellationToken cancellationToken)
+        {
+            return await _authService.VerifyEmailAsync(request.Token);
+        }
+    }
+}
