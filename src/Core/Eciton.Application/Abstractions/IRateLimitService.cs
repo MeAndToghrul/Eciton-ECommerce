61740159772bb58bb972dd31b2eb0736c8882abb
xaqan05 <xaqan2005@gmail.com>
2025-07-25 04:28:07 +0400

    CREATED RateLimiterService

---
+++ src/Core/Eciton.Application/Abstractions/IRateLimitService.cs
@@ -0,0 +1,6 @@
+﻿namespace Eciton.Application.Abstractions;
+public interface IRateLimitService
+{
+    Task<bool> IsBlocked(string ip);
+    Task RegisterFailedAttempt(string ip);
+}
---
+++ src/Infrastructure/Eciton.Persistence/Implements/RateLimitService.cs
@@ -0,0 +1,36 @@
+﻿using Eciton.Application.Abstractions;
+
+namespace Eciton.Persistence.Implements;
+public class RateLimitService : IRateLimitService
+{
+    private readonly ICacheService _cache;
+
+    public RateLimitService(ICacheService cache)
+    {
+        _cache = cache;
+    }
+
+    public async Task<bool> IsBlocked(string ip)
+    {
+        var value = await _cache.GetAsync<bool>($"BlockedIP_{ip}");
+        return value;
+    }
+
+    public async Task RegisterFailedAttempt(string ip)
+    {
+        string failKey = $"FailedLogin_{ip}";
+        string blockKey = $"BlockedIP_{ip}";
+
+        var value = await _cache.GetAsync<int>(failKey);
+        int count = value == default ? 0 : value;
+        count++;
+
+        await _cache.SetAsync(failKey, count, 1200); // saniye olaraq verdiyin üçün 10 dəq = 600
+
+        if (count >= 15)
+        {
+            await _cache.SetAsync(blockKey, true, 7200); // 1 gün = 86400 saniye
+            _cache.Delete(failKey);
+        }
+    }
+}
